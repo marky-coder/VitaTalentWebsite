6d725710deba9f9aaf16464f8d2610831c29add8
marky-coder <marky@svrea.co>
2025-12-26 07:26:20 -0500

    Create Nathaniel.jsx

---
+++ client/src/pages/Nathaniel.jsx
@@ -0,0 +1,15 @@
+// src/pages/Nathaniel.jsx
+import React from "react";
+import "./Nathaniel.css";
+
+export default function Nathaniel() {
+  return (
+    <div className="nathaniel-page">
+      <div className="container">
+        <div className="card">
+          <h2 className="card-title">NATHANIEL</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
