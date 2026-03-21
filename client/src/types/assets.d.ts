fd9d6931690c1eb0300c6ed4d6f419a8d1e46cd5
marky-coder <marky@svrea.co>
2026-03-21 04:34:12 +0000

    Create assets.d.ts

---
+++ client/src/types/assets.d.ts
@@ -0,0 +1,9 @@
+// client/src/types/assets.d.ts
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.mp4";
+declare module "*.mov";
